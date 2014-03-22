2e0ceb983d7cfc5aff683db6f4b93fef140a05d4
Peter-Smith <pt455888@dal.ca>
2014-03-22 16:41:01 -0300

    exits and rooms
    Exits connect rooms and are used to travel between them.

--- Exit.java
+++ Exit.java
@@ -5,5 +5,55 @@
 // When you type "north", you're really calling ( (player.location).north ).go!
 
 public class Exit {
+	public Exit(Room side1, Room side2) {
+		this.side1 = side1;
+		this.side2 = side2;
+		open = true;
+		
+	}
+	public Exit(Room side1, Room side2, String str) {
+		this.side1 = side1;
+		this.side2 = side2;
+		open = true;
+		switch (str){
+		case "ns":
+			side1.setNorth(this);
+			side2.setSouth(this);
+			break;
+		case "sn":
+			side2.setNorth(this);
+			side1.setSouth(this);
+			break;
+		case "ew":
+			side1.setEast(this);
+			side2.setWest(this);
+			break;
+		case "we":
+			side2.setEast(this);
+			side1.setWest(this);
+			break;
+		case "ud":
+			side1.setUp(this);
+			side2.setDown(this);
+			break;
+		case "du":
+			side2.setUp(this);
+			side1.setDown(this);
+			break;
+		}
+		
+	}
 	private Room side1, side2;
+	private boolean open;
+	private String closedReason; //If it's closed,
+	// this is the text that gets printed when you fail to travel across
+	
+	public void travel(){
+		if (open){
+		if (Game.player().getLocation()==side1){Game.player().setLocation(side2);}
+		else if (Game.player().getLocation()==side2){Game.player().setLocation(side1);}
+		// else, that is not how exits work.
+		}
+		else{Io.out(closedReason);}
+	}
 }
--- Room.java
+++ Room.java
@@ -6,4 +6,43 @@ public class Room {
 	public Thinglist getContents() {
 		return contents;
 	}
+	public void enter(){
+		Game.player().setLocation(this);
+	}
+	public Exit getNorth() {
+		return north;
+	}
+	public void setNorth(Exit north) {
+		this.north = north;
+	}
+	public Exit getSouth() {
+		return south;
+	}
+	public void setSouth(Exit south) {
+		this.south = south;
+	}
+	public Exit getWest() {
+		return west;
+	}
+	public void setWest(Exit west) {
+		this.west = west;
+	}
+	public Exit getEast() {
+		return east;
+	}
+	public void setEast(Exit east) {
+		this.east = east;
+	}
+	public Exit getDown() {
+		return down;
+	}
+	public void setDown(Exit down) {
+		this.down = down;
+	}
+	public Exit getUp() {
+		return up;
+	}
+	public void setUp(Exit up) {
+		this.up = up;
+	}
 }
