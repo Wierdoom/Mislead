fb807a9370e0f8f3eb13b3fb2902bb112cbdeb19
wierdoom <wierdoom@gmail.com>
2014-04-06 15:09:41 -0300

    It's all ogre now

---
+++ Onion.java
@@ -0,0 +1,15 @@
+public class Onion {
+   public boolean farquad;
+   public int layers;
+   
+   public boolean isFarquad() {
+      return farquad;
+   }
+   
+   public String (String shrek) {
+      if (shrekIsLove.equals("Shrek is love")) {
+         return "Shrek is life";
+      }
+      layers++;
+   }
+}
