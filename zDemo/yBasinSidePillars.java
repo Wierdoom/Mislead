e0163c33be4da250807479830b6954d29fa5700f
Peter-Smith <pt455888@dal.ca>
2014-04-11 19:38:37 -0300

    second-last type of basin room

---
+++ zDemo/yBasinSidePillars.java
@@ -0,0 +1,17 @@
+
+public class yBasinSidePillars extends yBasinSide {
+
+	public yBasinSidePillars(zWater water, zRain rain) {
+		super(water, rain, 2);
+		setLongDescription(getLongDescription()+"The remains of black pillars, pitted, pumice-like, stand here - three of them.");
+		setWetDesc("Even the largest fragments of the pillars that once stood here float and shift easily on the standing water.");
+	}
+	
+	public void describe(boolean full){
+		super.describe(full);
+		if (getContents().has("pillar")){
+			Io.out("Of four great pillars that stood here, one remains. Its squared black surface is aerated and pitted, its base almost worn away.");
+		}
+	}
+
+}
