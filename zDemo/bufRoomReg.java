70fa10f902f8a66363610fea825e3ad5b299924e
rice-guyenn <er879457@dal.ca>
2014-04-11 17:51:55 -0300

    Create bufRoomReg.java

---
+++ zDemo/bufRoomReg.java
@@ -0,0 +1,6 @@
+//basic empty rooms
+public class bufRoomReg extends WaterRoom {
+  public bufRoomReg(zWater water) {
+    super("Regular Room","An old room that is very clearly apart of the ruins you have entered into. Archaic designs and details litter the walls amidst overgrowth and ruin, but the room otherwise boasts nothing of interest.","A generic room leading deeper into the ruins.","The room is dry.","The room is wet and appears to be filling with water.","The room is now flooded",water);
+  }
+}
